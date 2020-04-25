45c08d06a47078f6ad7c52e001fe91f3e4ab1ece
jskriptus <skriptus@bk.ru>
2020-04-25 18:18:08 +0500

    Калькулятор доходов и рассходов v0.1a

---
+++ main.js
@@ -0,0 +1,103 @@
+const totalBalance = document.querySelector('.total__balance'),
+    totalMoneyIncome = document.querySelector('.total__money-income'),
+    totalMoneyExpenses = document.querySelector('.total__money-expenses'),
+    historyList = document.querySelector('.history__list'),
+    form = document.querySelector('#form'),
+    operationName = document.querySelector('.operation__name'),
+    operationAmount = document.querySelector('.operation__amount');
+
+let dbOperation = JSON.parse(localStorage.getItem('calc')) || [];
+
+
+const generateId = () => {
+    return `${Math.round(Math.random() * 1e8).toString(16)}`;
+};
+
+const renderOperation = (operation) => {
+    const classname = operation.amount < 0 ? 'history__item-minus' : 'history__item-plus';
+
+    const listItem = document.createElement('li');
+
+    listItem.classList.add('history__item');
+    listItem.classList.add(classname);
+
+    listItem.innerHTML = `${operation.description}
+        <span class="history__money">${operation.amount} ₽</span>
+        <button class="history_delete" data-id="${operation.id}">x</button>
+    `;
+
+    historyList.insertAdjacentElement('beforeend', listItem);
+};
+
+const updateBalance = () => {
+    const resultIncome = dbOperation
+        .filter((item) => item.amount > 0)
+        .reduce((result, item) => result + item.amount, 0);
+    totalMoneyIncome.textContent = resultIncome + '₽';
+
+    const resultExpenses = dbOperation
+        .filter((item) => {
+            return item.amount < 0;
+        })
+        .reduce((result, item) => result + item.amount, 0);
+    totalMoneyExpenses.textContent = resultExpenses + '₽';
+
+    totalBalance.textContent = (resultIncome + resultExpenses) + '₽';
+};
+
+const addOperation = (event) => {
+    event.preventDefault();
+    const operationNameValue = operationName.value,
+        operationAmountValue = operationAmount.value;
+
+    operationName.style.borderColor = '';
+    operationAmount.style.borderColor = '';
+
+    if (operationNameValue && operationAmountValue) {
+        operationName.value = '';
+        operationAmount.value = '';
+
+        const operation = {
+            id: generateId(),
+            description: operationNameValue,
+            amount: +operationAmountValue
+        };
+
+        dbOperation.push(operation);
+
+        init();
+    } else {
+        if (!operationNameValue) {
+            operationName.style.borderColor = 'red';
+        }
+        if (!operationAmountValue) {
+            operationAmount.style.borderColor = 'red';
+        }
+    }
+};
+
+const deleteOperation = (event) => {
+    const target = event.target;
+    if (target.classList.contains('history_delete')) {
+        dbOperation = dbOperation.filter((operation) => operation.id !== target.dataset.id);
+
+        init();
+    }
+};
+
+const init = () => {
+    historyList.textContent = '';
+
+    dbOperation.forEach((item) => {
+        renderOperation(item);
+    });
+
+    updateBalance();
+
+    localStorage.setItem('calc', JSON.stringify(dbOperation));
+};
+
+form.addEventListener('submit', addOperation);
+historyList.addEventListener('click', deleteOperation);
+
+init();
